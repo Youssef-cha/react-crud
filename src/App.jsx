dd7ba213537990cd7452b4d974aae22983a0fb1c
Youssef-cha <youssef.charroud445@gmail.com>
2024-12-28 13:38:20 +0100

    before pass functions as props part

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,13 @@ import JobsPage from "./pages/JobsPage";
 import MainLayout from "./layouts/MainLayout";
 import NotFound from "./pages/NotFound"
 import JobPage , {jobLoader} from "./pages/JobPage";
-
+import AddJobPage from "./pages/AddJobPage";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/add-job" element={<AddJobPage />} />
       <Route loader={jobLoader} path="/jobs/:id" element={<JobPage />} />
       <Route path="*" element={<NotFound />} />
     </Route>
--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -7,7 +7,9 @@ const JobListings = ({ isHome = false }) => {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchJobs = async () => {
-      const apiUrl = isHome ? "http://localhost:8000/jobs?_limit=3" : "http://localhost:8000/jobs";
+      const apiUrl = isHome
+        ? "http://localhost:8000/jobs?_limit=3"
+        : "http://localhost:8000/jobs";
       try {
         const res = await fetch(apiUrl);
         const data = await res.json();
@@ -30,13 +32,11 @@ const JobListings = ({ isHome = false }) => {
         {loading ? (
           <Spinner loading={loading} />
         ) : (
-          <>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {jobs.map((job) => (
-                <JobListing key={job.id} job={job} />
-              ))}
-            </div>
-          </>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
         )}
       </div>
     </section>
